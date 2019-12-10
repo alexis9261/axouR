4efd5247b2337df0d27b275aa75007199f99727c
alexis9261 <alexisamm9261@gmail.com>
2019-12-10 12:17:49 -0400

    a

--- perfil/compras.php
+++ perfil/compras.php
@@ -120,18 +120,17 @@ if($res->num_rows>0){
               $estatusPago=$row4['ESTATUS'];
               if($estatusPago==0){
                 $colorPago="primary";
-                $svg_pago="<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 384 512' width='10px'><path fill='#006fcb' d='M360 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24 0 90.965 51.016 167.734 120.842 192C75.016 280.266 24 357.035 24 448c-13.255 0-24 10.745-24 24v16c0 13.255 10.745 24 24 24h336c13.255 0 24-10.745 24-24v-16c0-13.255-10.745-24-24-24 0-90.965-51.016-167.734-120.842-192C308.984 231.734 360 154.965 360 64c13.255 0 24-10.745 24-24V24c0-13.255-10.745-24-24-24zm-75.078 384H99.08c17.059-46.797 52.096-80 92.92-80 40.821 0 75.862 33.196 92.922 80zm.019-256H99.078C91.988 108.548 88 86.748 88 64h208c0 22.805-3.987 44.587-11.059 64z'/></svg>";
+                $title_pago="Pago esperando a ser confirmado";
               }elseif($estatusPago==1){
                 $colorPago="success";
-                $svg_pago="<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512' width='10px'><path fill='#00ca04' d='M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z'/></svg>";
+                $title_pago="Pago Confirmado";
               }else{
                 $colorPago="danger";
-                $svg_pago="<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 192 512' width='10px'><path d='M176 432c0 44.112-35.888 80-80 80s-80-35.888-80-80 35.888-80 80-80 80 35.888 80 80zM25.26 25.199l13.6 272C39.499 309.972 50.041 320 62.83 320h66.34c12.789 0 23.331-10.028 23.97-22.801l13.6-272C167.425 11.49 156.496 0 142.77 0H49.23C35.504 0 24.575 11.49 25.26 25.199z'/></svg>";
+                $title_pago="Pago no confirmado";
               }
               ?>
-              <div class="row text-<?php echo $colorPago;?> justify-content-end" title="" data-toggle="">
+              <div class="row text-<?php echo $colorPago;?> justify-content-end" title="<?php echo $title_pago;?>" data-toggle="tooltip">
                 <?php echo number_format($pago,2,',','.')." $moneda";?>
-                <?php echo $svg_pago;?>
               </div>
               <?php
             }
