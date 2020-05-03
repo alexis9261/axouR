3c5525cf22578813ea0fd78fe2b57ad475fd3af3
root <root@eutuxiagroup.com>
2020-05-03 17:16:08 -0300

    pruebas en el servidos

--- common/conexion.php
+++ common/conexion.php
@@ -1,21 +1,21 @@
 <?php
-$servername="localhost";
+$servername="66.97.37.59";
 //servidor
 /*$username="id4956438_rouxa";
 $password="rouxarouxabd";
 $dbname="id4956438_rouxa";*/
 //localhost
-$username="root";
-$password="";
-$dbname="rouxa";
+$username="carlos";
+$password="Cm20184865";
+$dbname="ROUXA";
 // Create connection
 $conn=new mysqli($servername,$username,$password,$dbname);
 $conn->set_charset("utf8");
 if($conn->connect_error){die("Connection failed: ".$conn->connect_error);}
 //defino una variable donde se guardara la direccion inicial del proyecto
 //--local--
-$root_folder="http://localhost/Rouxa/";
+//$root_folder="http://localhost/Rouxa/";
 //--para servidor--
-//$root_folder=""; // prints '/home/public_html/'
+$root_folder=""; // prints '/var/www/axouR/'
 $nombrePagina="Rouxa";
 $imageLogo="logo.png";
